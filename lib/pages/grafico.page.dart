8a2973930c0c82fcbce3d9f32916a0775d9669c7
Thiago Neves <thiagosantostneves@hotmail.com>
2020-11-23 20:32:29 -0300

    last commit

--- lib/pages/grafico.page.dart
+++ lib/pages/grafico.page.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class GraficoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-          title: new Text("Grafico"),
+        title: new Text("Grafico"),
+      ),
+      body: Container(
+        child: WebView(
+          initialUrl: Uri.dataFromString(
+                  '<html><body><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/1232722/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=spline"></iframe></body></html>',
+                  mimeType: 'text/html')
+              .toString(),
+          javascriptMode: JavascriptMode.unrestricted,
         ),
-        body: Center(child: Image.asset('assets/grafico2.png'))
+      ),
     );
   }
 }
